730e8cf3b77014c74fe629881079b3568472b157
Karim <cheqrounikarim@gmail.com>
2019-09-28 18:10:15 +0000

    fc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Produit } from './list-produit/produit';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public produit: Produit;
+
   title = ' Suivi du Matériel';
 }
